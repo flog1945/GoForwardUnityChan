0ffa1788c90643756f0786e6c22c10ad347fbf3d
坂田 玄樹 <ginga777@cnc.bbiq.jp>
2018-02-08 19:46:52 +0900

    GoForwardUnityChan 課題
    Lesson7の課題となります。

---
+++ Assets/CubeSE.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSE : MonoBehaviour {
+
+    //効果音
+    private AudioClip block;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            audioSource.Play();
+    }
+}
